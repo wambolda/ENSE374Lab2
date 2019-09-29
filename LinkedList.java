b4650b43894f8bfa8419179de70dd06fae6622c8
Allan Wambold <wambolda@uregina.ca>
2019-09-29 13:19:10 -0600

    Comments added

--- LinkedList.java
+++ LinkedList.java
@@ -3,6 +3,14 @@ public class LinkedList
 	ListElement head = new ListElement();
 	int numNodes = 0;
 	
+	// Purpose: add an element to the linked list
+	// Parameter(s): 
+	// (1) ListElement le - ListElemenet to be added to the list
+	// Precondition(s): N/A
+	// Returns: void
+	// Side effect(s): 
+	// (1) adds le to the end of the linked list
+	// (2) changes the former last ListElement next value to be that of le
 	public void addElement(ListElement le)
 	{
 		if(numNodes == 0)
@@ -23,6 +31,14 @@ public class LinkedList
 		return;
 	}
 	
+	// Purpose: Retrieve a ListElement from the linked list given the elements index number
+	// Parameter(s): 
+	// (1) int index - index number of the ListElement to retrieve
+	// Precondition(s): 
+	// (1) index number must be valid
+	// Returns: ListElement - the ListElement located at the provided index
+	// Side effect(s): 
+	// (1) Returns a blank ListElement if the provided index number is invalid
 	public ListElement getElement(int index)
 	{
 		if(index > numNodes)
@@ -43,7 +59,7 @@ public class LinkedList
 		}
 	}
 	
-	//public ListElement deleteElement(int index)
+	public ListElement deleteElement(int index)
 	{
 		
 	}

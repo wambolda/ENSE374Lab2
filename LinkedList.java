127355c2d4bf30ef561dd553041486288c91afd8
Allan Wambold <wambolda@uregina.ca>
2019-09-29 12:13:33 -0600

    Commented unnecessary lines in ListElement class created addNode method in LinkedList class

--- LinkedList.java
+++ LinkedList.java
@@ -1,6 +1,53 @@
 public class LinkedList
 {
+	ListElement head = new ListElement();
+	int numNodes = 0;
+	
+	public void addNode(ListElement le)
+	{
+		if(numNodes == 0)
+		{
+			head = le;
+			numNodes++;
+		}
+		else
+		{
+			ListElement temp = this.head;
+			while(temp.getNext() != null)
+			{
+				temp = temp.getNext();
+			}
+			temp.setNext(le);
+			numNodes++;
+		}
+		return;
+	}
+	
+	public ListElement getElement(int index)
+	{
+		
+	}
+	
+	public ListElement deleteElement(int index)
+	{
+		
+	}
+	
+	public void printLinkedListTail()
+	{
+		
+	}
+	
+	public void printLinkedListHead()
+	{
+		
+	}
 
 
-
+	public static void main(String[] args)
+	{
+		ListElement le = new ListElement();
+		le.setData(5);
+		//LinkedList myList = new LinkedList();
+	}
 }
